c68e559391e263b86ba1d3b10700ad804372e8cc
alexjcjunior <alexjcj.12@gmail.com>
2021-12-13 10:21:17 -0300

    mobile finall

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:http/http.dart' as http;
+
+import 'dart:convert' as convert;
+
+void main() async{
+  var url =
+  Uri.http('192.168.25.48:5000', '/produto/Cadastrar');
+
+
+
+  // Await the http get response, then decode the json-formatted response.
+  var response = await http.get(Uri.parse('http://192.168.25.48:5000/Produto/Listar'));
+  print('cadastrado com sucesso!');
+
+    var resposta = await http.post(url, body: {
+    "id": '1',
+    "nome": "biscoito agua",
+    "preço": '4.99',
+});
+
+  print(response.statusCode);
+  print(url);
+  // var respostaConvertida = convert.jsonDecode(resposta.body) as List<dynamic>;
+  //Map teste = convert.jsonDecode(respostaConvertida[0]);
+
+  // print(url);
+}
+
+
